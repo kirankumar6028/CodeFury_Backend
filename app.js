872f99a4650080b7836f63fd589ddb0fc46c2d67
kirankumar6028 <kiran6028kumar@gmail.com>
2020-09-22 18:58:08 +0530

    project created

---
+++ app.js
@@ -0,0 +1,16 @@
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+
+app.set("view engine","ejs");
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(express.static("public"));
+
+
+
+app.listen(3000, function(){
+    console.log("server is running on port 3000");
+});
